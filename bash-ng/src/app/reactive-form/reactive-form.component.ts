1ba3a67a5d0b54e05a441d9fd79ce83837e6c83f
Ahmed Aoulad Amar <71600486+Ahmed-dev-dragon@users.noreply.github.com>
2022-07-07 10:28:37 +0100

    aad

--- bash-ng/src/app/reactive-form/reactive-form.component.ts
+++ bash-ng/src/app/reactive-form/reactive-form.component.ts
@@ -1,5 +1,6 @@
 import { NgClass } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+
 import {
   FormBuilder,
   FormControl,
@@ -16,8 +17,24 @@ import { NgModel } from '@angular/forms';
 })
 export class ReactiveFormComponent implements OnInit {
   control = new FormControl();
+  controll = new FormControl();
   model = 7;
-  constructor() {}
+  controltow = new FormControl();
+
+  name = new FormControl();
+  a!: any;
+  porfileForm = new FormGroup({
+    controltoww: new FormControl(),
+    modeltow: new FormControl(),
+  });
+
+  constructor() {
+    this.name.valueChanges.subscribe((changes) => (changes = this.controltow));
+  }
 
   ngOnInit(): void {}
+  onSubmit() {
+    this.a = this.porfileForm;
+    return this.a; 
+  }
 }
