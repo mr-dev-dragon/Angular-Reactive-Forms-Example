93e62a6ed9f100f45bbc7a469508c28bdab62aab
Ahmed Aoulad Amar <71600486+Ahmed-dev-dragon@users.noreply.github.com>
2022-07-08 12:12:25 +0100

    add val

--- bash-ng/src/app/reactive-form/reactive-form.component.ts
+++ bash-ng/src/app/reactive-form/reactive-form.component.ts
@@ -45,25 +45,25 @@ export class ReactiveFormComponent implements OnInit {
   constructor(private fb: FormBuilder) {
     this.addForm = this.fb.group({
       First_name: ['',
-        [Validators.required, Validators.maxLength(10)], []],
+        [Validators.required, Validators.maxLength(10)]],
       Last_name: ['',
-        [Validators.required, Validators.maxLength(10)], []],
+        [Validators.required, Validators.maxLength(10)]],
       Email: ['',
-        [Validators.required, Validators.maxLength(10)], []],
+        [Validators.required, Validators.maxLength(10)]],
       Password: ['',
-        [Validators.required, Validators.maxLength(10)], []],
+        [Validators.required, Validators.maxLength(10)]],
       Address: ['',
-        [Validators.required, Validators.maxLength(10)], []],
+        [Validators.required, Validators.maxLength(10)]],
       Address_2: ['',
-        [Validators.required, Validators.maxLength(10)], []],
+        [Validators.required, Validators.maxLength(10)]],
       City: ['',
-        [Validators.required, Validators.maxLength(10)], []],
+        [Validators.required, Validators.maxLength(10)]],
       State: ['',
-        [Validators.required, Validators.maxLength(10)], []],
+        [Validators.required, Validators.maxLength(10)]],
       Zip: ['',
-        [Validators.required, Validators.maxLength(10)], []],
+        [Validators.required, Validators.maxLength(10)]],
       Check_me_out: ['',
-        [Validators.required, Validators.maxLength(10)], []],
+        [Validators.required, Validators.maxLength(10)]]
     });
     this.addForm.valueChanges.subscribe(console.log);
   }
